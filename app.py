925bc2c45ac9fa64225778ed4101338b31f59493
Gus Cavanaugh <Gus Cavanaugh>
2016-08-13 16:59:01 -0400

    returning weather

--- app.py
+++ app.py
@@ -14,14 +14,21 @@ app = create_app()
 @app.route('/', methods = ['GET','POST'])
 def index():
 	body = request.values.get('Body', None)
-	mylat = body.split(",")[0]
-	mylong = body.split(",")[1]
+	try:
+		mylat = body.split(",")[0]
+		mylong = body.split(",")[1]
+	except AttributeError:
+		message = "Please provide a single lat & long separated by a comma, e.g., 12.0, -8.0"
+		resp = twilio.twiml.Response()
+		resp.message(message)
+		return str(resp)
 	try:
 		action = get_action(mylat, mylong)
 		df = pd.DataFrame(action)
 		message = df[['date', 'action']].to_string(index=False)
 	except:
-		message = "Please provide a single lat/long separated by a comma, e.g., 12.0, -8.0"
+		message = """I couldn't parse your lat/long, please be sure to only type a comma
+					separated lat and long, e.g., 12.0, -8.0"""
 
 	resp = twilio.twiml.Response()
 	resp.message(message)
